c25eccf083f808f319d3ac7af2a89d5b4cd4c8af
RobinHankin <hankin.robin@gmail.com>
2019-04-11 15:24:03 +1200

    another test

--- tests/aaa.R
+++ tests/aaa.R
@@ -10,3 +10,7 @@ stopifnot(is.zero(spray(matrix(c(1,1,2,2),2,2),c(1,-1),addrepeats=TRUE)))
 
 # Following test removes zero from not-the-last row of index matrix M:
 ignore <- spray(matrix(c(1,2,1,2,3,3),byrow=T,ncol=2),c(1,-1,10),addrepeats=TRUE)
+
+# Following test checks a big-ish spray where multiple index rows
+# cancel to zero:
+stopifnot(is.zero(spray(kronecker(matrix(1,16,4),1+diag(2)),rep(c(1,1,-1,-1),length=32),addrepeats=TRUE)))
