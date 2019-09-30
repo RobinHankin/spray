da2dd7a0a4a5539c06fc0601c5ef877735fdefa8
RobinHankin <hankin.robin@gmail.com>
2019-09-30 17:42:39 +1300

    better tests, using a loop

--- tests/testthat/test_four_square.R
+++ tests/testthat/test_four_square.R
@@ -1,6 +1,7 @@
 ## Four-square identity for sprays
 
 test_that("four-square identity",{
+    # first a cute test:
     LHS <- spray(cbind(diag(2,4),diag(0,4)))*spray(cbind(diag(0,4),diag(2,4)))
 
     RHS <-
@@ -12,4 +13,37 @@ test_that("four-square identity",{
     expect_true(is.zero(LHS-RHS))
     expect_true(is.zero(RHS-LHS))
     expect_true(LHS == RHS)
+
+
+    # Now a very tough test:
+    foo <- function(
+                    a1,a2,a3,a4,
+                    b1,b2,b3,b4)
+    {
+        LHS <- (a1^2+a2^2+a3^2+a4^2)*(b1^2+b2^2+b3^2+b4^2)
+        RHS <- (
+            (a1*b1-a2*b2-a3*b3-a4*b4)^2 +
+            (a1*b2+a2*b1+a3*b4-a4*b3)^2 +
+            (a1*b3-a2*b4+a3*b1+a4*b2)^2 +
+            (a1*b4+a2*b3-a3*b2+a4*b1)^2
+        )
+
+    expect_true(is.zero(LHS-RHS))
+    expect_true(is.zero(RHS-LHS))
+    expect_true(LHS == RHS)
+    }
+
+    for(i in 1:4){
+        foo(
+            rspray(2,powers=0:9),
+            rspray(2,powers=0:9),
+            rspray(2,powers=0:9),
+            rspray(2,powers=0:9),
+            rspray(2,powers=0:9),
+            rspray(2,powers=0:9),
+            rspray(2,powers=0:9),
+            rspray(2,powers=0:9)
+        )
+    }
 })
+
