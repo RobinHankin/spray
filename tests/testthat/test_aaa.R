5058fb3c1eb16350218ee28d98cd6e5c81f27436
RobinHankin <hankin.robin@gmail.com>
2019-10-12 14:07:43 +1300

    more tests to increase coverage

--- tests/testthat/test_aaa.R
+++ tests/testthat/test_aaa.R
@@ -59,5 +59,39 @@ test_that("test suite aaa",{
 
     expect_error(S1[rspray(n=3,arity=4)])
 
+    expect_silent(S1[] <- 3)
+    expect_silent(S1[] <- S2)
+    expect_silent(S1[diag(3)] <- 3)
+    expect_silent(S1[S2] <- 3)
+    expect_silent(S2[1:2,1:2,1:2] <- 55)
+    expect_error(S2[1:2,1:2] <- 55)
+    expect_silent(S2[1:2,1:2,1:2] <- 55)
+    expect_error(S2[1:2,1:2,1:2,1:2] <- 55)
+    
+    S1 <- spray(matrix(sample(-2:2,replace=TRUE,21),ncol=3),rnorm(7),addrepeats=TRUE)
+    S2 <- spray(matrix(sample(-2:2,replace=TRUE,15),ncol=3),rnorm(5),addrepeats=TRUE)
+    
+    f1 <- as.function(S1)
+    f2 <- as.function(S2)
+    f3 <- as.function(S1*S2)
+    x <- 4:6
+    
+    expect_equal(f1(x)*f2(x),f3(x))
+    
+    expect_silent(constant(S1) <- 3)
+
+    expect_equal(S1,S1+zero(3))
+     
+
+    expect_silent(linear(1:4)+lone(3,4)+xyz(4)+one(4))
+    expect_silent(one(rspray(9)))
+
+    a <- homog(4,2)
+    jj <-  (1-a)*ooom(a,3)
+    expect_true(constant(jj,drop=TRUE)==1)
+    expect_true(sum(rowSums(index(jj))==0)==1) 
+    expect_true(all(rowSums(index(jj)) %in% c(0,8)))
+
+    
 })
 
