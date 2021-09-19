75da4c11dc126105348a15761e8459ce1b3201a1
Robin Hankin <rhankin@aut.ac.nz>
2021-09-19 22:06:58 +1200

    make package name larger

--- inst/spray_stickermaker.R
+++ inst/spray_stickermaker.R
@@ -10,7 +10,7 @@ plot(NA,xlim=c(0,1),ylim=c(-1,1),axes=FALSE,xlab="",ylab="",asp=1)
 for(angle in seq(from=-0.6,to=0.6,len=7)){plotseq(angle)}
 dev.off()
 
-sticker("spray_icon.png", package="spray", p_size=8, s_x=0.9, s_y=0.95,
+sticker("spray_icon.png", package="spray", p_size=24, s_x=0.9, s_y=0.95,
 s_width=2, asp=0.85, white_around_sticker=TRUE, h_fill="#7733FF",
 h_color="#000000", filename="spray.png")
 
