339509c519f7802c79496ebd3f6d9d912301c1f0
Robin Hankin <hankin.robin@gmail.com>
2021-09-21 20:55:13 +1200

    orientation of spray

--- inst/spray_stickermaker.R
+++ inst/spray_stickermaker.R
@@ -2,15 +2,17 @@ library("hexSticker")
 
 plotseq <- function(angle){
   v <- seq(from=0,to=1,len=10)
-  points(v*cos(angle),v*sin(angle),pch=16,cex=2*sqrt(1-v))
+  points(v*sin(angle),-v*cos(angle),pch=16,cex=3.5*sqrt(1-v))
 }
 
 png(file="spray_icon.png",width=1000,height=1000,bg="#7733FF")
-plot(NA,xlim=c(0,1),ylim=c(-1,1),axes=FALSE,xlab="",ylab="",asp=1)
+par(mai=c(0,0,0,0))
+plot(NA,xlim=c(-1,1),ylim=c(-1,1),axes=FALSE,xlab="",ylab="",asp=1)
 for(angle in seq(from=-0.6,to=0.6,len=7)){plotseq(angle)}
+# abline(v=0,lwd=4)  # here for centering
 dev.off()
 
-sticker("spray_icon.png", package="spray", p_size=24, s_x=0.9, s_y=0.95,
+sticker("spray_icon.png", package="spray", p_size=24, s_x=1, s_y=1.8,
 s_width=2, asp=0.85, white_around_sticker=TRUE, h_fill="#7733FF",
 h_color="#000000", filename="spray.png")
 
